72045e05c86d51e723a9a5c78e981ce36e153dbf
JOSE <josedilla77@gmail.com>
2026-02-18 12:23:39 +0100

    Mi primer código Java SGI

---
+++ servidor-incidencias/servidor-incidencias/src/main/java/com/escuela/servidor_incidencias/ServidorIncidenciasApplication.java
@@ -0,0 +1,13 @@
+package com.escuela.servidor_incidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorIncidenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorIncidenciasApplication.class, args);
+	}
+
+}
---
+++ servidor-incidencias/servidor-incidencias/src/test/java/com/escuela/servidor_incidencias/ServidorIncidenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escuela.servidor_incidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorIncidenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
